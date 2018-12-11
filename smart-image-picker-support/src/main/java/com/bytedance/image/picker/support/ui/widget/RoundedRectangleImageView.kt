d374f0397b406cd43ae4f5c47bb6d9850906df0c
zlove <zlove.zhang@bytedance.com>
2018-12-11 16:13:49 +0800

    Add Widget

---
+++ smart-image-picker-support/src/main/java/com/bytedance/image/picker/support/ui/widget/RoundedRectangleImageView.kt
@@ -0,0 +1,45 @@
+package com.bytedance.image.picker.support.ui.widget
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Path
+import android.graphics.RectF
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageView
+
+class RoundedRectangleImageView: AppCompatImageView {
+
+    private var mRadius: Float = 0.toFloat() // dp
+    private var mRoundedRectPath: Path? = null
+    private var mRectF: RectF? = null
+
+    constructor(context: Context) : super(context) {
+        init(context)
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init(context)
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init(context)
+    }
+
+    private fun init(context: Context) {
+        val density = context.resources.displayMetrics.density
+        mRadius = 2.0f * density
+        mRoundedRectPath = Path()
+        mRectF = RectF()
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        mRectF!!.set(0.0f, 0.0f, width.toFloat(), height.toFloat())
+        mRoundedRectPath!!.addRoundRect(mRectF, mRadius, mRadius, Path.Direction.CW)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        canvas.clipPath(mRoundedRectPath!!)
+        super.onDraw(canvas)
+    }
+}
